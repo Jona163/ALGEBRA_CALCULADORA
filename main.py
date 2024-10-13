d95fd5bd502f8466475f7efb08aaa33f4daebc1d
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-12 21:44:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -27,3 +27,35 @@ def switch(case, q, w, e, r):
 
 def default():
     return "La opcion %s no existe en el menu"% case
+
+repetir=True
+while repetir:
+    print("**************Calculadora de Numeros Complejos**************")
+    print("Numeros de la forma a+bi")
+    print("Con a como la parte real y b la parte imaginaria")
+    print("************************************************************")
+    menu()
+    case=int(input("Selecciona una operacion "))
+    if case == 1:
+        a=int(input("Digite la parte real del primer numero complejo "))
+        b=int(input("Digite la parte imaginaria del primer numero complejo "))
+        z = complex(a,b)
+        print("Numero ingresado: ",(z))
+        c=int(input("Digite la parte real del segundo numero complejo "))
+        d=int(input("Digite la parte imaginaria del segundo numero complejo "))
+        z = complex(c,d)
+        print("Numero ingresado: ",(z))
+        n=a+c
+        m=b+d
+        z= complex(n, m)
+        print ("El resultado es", z)
+        x = [n]
+        y = [m]
+        plt.scatter(x,y)
+        plt.ylabel('Imaginario')
+        plt.xlabel('Real')
+        plt.title("Gráfica cartesiana",  fontsize=15, color="r", name="Times New Roman")
+        plt.show()
+        rep=input("¿Desea realizar otra operacion? [Si/No]")
+        if rep == "si" or rep == "Si":
+                repetir = True
