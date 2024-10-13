7f28c29cf2dc29a77159dc147b2ce054424e84ed
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-12 21:46:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -114,3 +114,13 @@ while repetir:
                 repetir = True
         else:
                 repetir=False
+
+    elif case == 4:
+        a=int(input("Digite la parte real del primer numero complejo "))
+        b=int(input("Digite la parte imaginaria del primer numero complejo "))
+        z = complex(a,b)
+        print("Numero ingresado: ",(z))
+        c=int(input("Digite la parte real del segundo numero complejo "))
+        d=int(input("Digite la parte imaginaria del segundo numero complejo "))
+        z = complex(c,d)
+        print("Numero ingresado: ",(z))
